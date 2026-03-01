08ab009830433a92ddbb07652596a12379c19ad7
v-rogana <v-rogana@gmail.com>
2026-03-01 00:53:39 -0300

    Preparando para o GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/descubra-sua-casa/', // Substitua pelo nome real que dará ao repositório no GitHub
 })
